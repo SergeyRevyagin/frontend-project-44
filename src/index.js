4f6154da7556635f19f93f1166db74115d056607
SergeyRevyagin <west1995@mail.ru>
2023-04-24 15:06:16 +0300

    added game logic

---
+++ src/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const roundsCount = 3;
+
+const gameLogic = (generateRound, gameDescription) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(gameDescription);
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, correctAnswer] = generateRound();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default gameLogic;
