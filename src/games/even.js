722f49a623776917529db5c546829bc51e41cf71
SergeyRevyagin <west1995@mail.ru>
2023-04-24 15:01:54 +0300

    fix.moved the game logic to a separate file called index

--- src/games/even.js
+++ src/games/even.js
@@ -1,49 +1,21 @@
-import readlineSync from 'readline-sync';
-import welcome from '..//cli.js';
+import getRandom from '../getRandom.js';
+import gameLogic from '../index.js';
 
-export default () =>{
-    const min = 0;//значения можно поменять
-    const max = 100;//значения можно поменять
-    const f = true;
-    let index = 0;
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const name = welcome();
-const getRandomIndex = (min,max) => Math.floor(Math.random() * (max - min) + min);
+const isEven = (number) => number % 2 === 0;
 
-//определяем четное число или нет. 
-const numIsEven = (number) => (number % 2 === 0);
+const startBrainEvenCore = () => {
+  const randomInt = getRandom(1, 101);
+  const question = `${randomInt}`;
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-while(index < 3){
-if(index < 3){
-const number = getRandomIndex(min,max);
-console.log(`Question: ${number}`);//выводим рандомное число
-const answer = readlineSync.question('Yout answer: ');
-if(answer ==='yes' && numIsEven(number) === true ){
-console.log(`Correct!`);
-index += 1;
-}else if(answer ==='no' && numIsEven(number) === false){
-console.log(`Correct!`);
-index += 1;
-}else{
-    if(answer === 'no'){
-        console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.
-        Let's try again, ${name}!`);
-        break;
-    
-    }else if(answer === 'yes'){
-        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.
-        Let's try again, ${name}!`);
-        break;
-    }else{
-        console.log(`sorry, wrong choice..Let's try again, ${name}!`)
-        break;
-    }
-    
-}
-}if(index === 3){
-    console.log(`Congratulations, ${name}`);
-}
-}
+  const correctAnswer = isEven(randomInt) ? 'yes' : 'no';
 
-}
+  return [question, correctAnswer];
+};
+
+const start = () => {
+  gameLogic(startBrainEvenCore, gameDescription);
+};
+
+export default start;
